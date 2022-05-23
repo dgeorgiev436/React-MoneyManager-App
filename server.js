3a997d95948dd67d89c146f1a5f6b5153b4f6bca
dgeorgiev436 <68106371+dgeorgiev436@users.noreply.github.com>
2022-05-23 15:39:57 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path")
 
 // Connect the database
 connectDB();
 
 app.use(express.json({extended: false}));
 
-app.get("/", (req,res) => {
-	res.send("Home Page")
-})
-
 app.use("/api/users", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/expenses", require("./routes/api/expenses"));
 
+// Serve static assets in production
 
+if(process.env.NODE_ENV === "production"){
+	// Set static folder
+	app.use(express.static("client/build"));
 
+	app.get("*", (req,res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	})
+}
 
 const PORT = process.env.PORT || 5000
 
